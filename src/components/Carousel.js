5546141eb67d5b76a2cacc57f546664530219947
Sean Zhou <51042748+xiaoShuZhou@users.noreply.github.com>
2023-02-26 22:16:19 +0200

    final

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,23 +5,48 @@ import "slick-carousel/slick/slick-theme.css";
 import Item from './Item';
 
 const Carousel = ({ items }) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-  };
+ const settings = {
+   dots: true,
+   infinite: true,
+   speed: 500,
+   slidesToShow: 4,
+   slidesToScroll: 4,
+   responsive: [
+       {
+         breakpoint: 1024,
+         settings: {
+           slidesToShow: 3,
+           slidesToScroll: 3,
+           infinite: true,
+           dots: true
+         }
+       },
+       {
+         breakpoint: 600,
+         settings: {
+           slidesToShow: 2,
+           slidesToScroll: 2,
+           initialSlide: 2
+         }
+       },
+       {
+         breakpoint: 480,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1
+         }
+       }
+     ]
+ };
 
-  return (
-    <div>
-      <Slider {...settings}>
-        {items.map(item => (
-          <Item key={item.id} {...item} />
-        ))}
-      </Slider>
-    </div>
-  );
+
+ return (
+     <Slider {...settings}>
+       {items.map(item => (
+         <Item key={item.id} {...item} />
+       ))}
+     </Slider>
+ );
 };
 
 
